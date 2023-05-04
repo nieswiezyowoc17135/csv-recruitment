9775d44f6764ae4096dc8389a39acc7e85b042a2
nieswiezyowoc17135 <nieswiezyowoc17135@wp.pl>
2023-05-05 00:01:49 +0200

    WIP.

--- csv-project/Features/Query/CsvReadQuery.cs
+++ csv-project/Features/Query/CsvReadQuery.cs
@@ -12,34 +12,36 @@ public class CsvReadQueryValidator : AbstractValidator<CsvReadQuery>
 {
     public CsvReadQueryValidator()
     {
+        RuleFor(x => x)
+            .Must(x => x.ClientCodes!.Count == 0 && x.OrderNumber == null)
+            .When(x => x.FromWhen != null || x.ToWhen != null)
+            .WithMessage("If Dates are passed, other arguments have to be null.");
 
-        RuleFor(x => x.OrderNumber)
-            .Cascade(CascadeMode.Stop)
-            .NotNull()
-            .When(x => x.ClientCodes == null || x.FromWhen == null || x.ToWhen == null)
-            .WithMessage("OrderNumber must not be null if other arguments are null.");
+        RuleFor(x => x)
+            .Must(x => x.OrderNumber == null && x.FromWhen == null && x.ToWhen == null)
+            .When(x => x.ClientCodes!.Count > 0)
+            .WithMessage("If ClientCodes are passed, other arguments have to be null.");
 
-        RuleFor(x => x.ClientCodes)
-            .Cascade(CascadeMode.Stop)
-            .NotNull()
-            .When(x => x.OrderNumber == null || x.FromWhen == null || x.ToWhen == null)
-            .WithMessage("ClientCodes must not be null if other arguments are null.");
+        RuleFor(x => x)
+            .Must(x => x.ClientCodes!.Count == 0 && x.FromWhen == null & x.ToWhen == null)
+            .When(x => x.OrderNumber != null)
+            .WithMessage("If Order Number is passed, other arguments have to be null.");
 
         RuleFor(x => x.FromWhen)
             .Cascade(CascadeMode.Stop)
             .NotNull()
-            .When(x => x.OrderNumber == null || x.ClientCodes == null || x.ToWhen != null)
+            .When(x => x.OrderNumber == null && x.ClientCodes!.Count == 0 && x.ToWhen != null)
             .WithMessage("FromWhen must not be null if ToWhen is not null and other arguments are null.");
 
         RuleFor(x => x.ToWhen)
             .Cascade(CascadeMode.Stop)
             .NotNull()
-            .When(x => x.OrderNumber == null || x.ClientCodes == null || x.FromWhen != null)
+            .When(x => x.OrderNumber == null && x.ClientCodes!.Count ==0 && x.FromWhen != null)
             .WithMessage("ToWhen must not be null if FromWhen is not null and other arguments are null.");
 
         RuleFor(x => x)
             .Cascade(CascadeMode.Stop)
-            .Must(x => x.OrderNumber != null || x.ClientCodes != null || (x.FromWhen != null && x.ToWhen != null))
+            .Must(x => x.OrderNumber != null || x.ClientCodes!.Count > 0 || (x.FromWhen != null && x.ToWhen != null))
             .WithMessage("At least one of OrderNumber, ClientCodes, or FromWhen/ToWhen must be specified.");
 
         RuleFor(x => x)
@@ -63,20 +65,13 @@ public class CsvReaderClass
             //validation
             var validator = new CsvReadQueryValidator();
             var result = validator.Validate(request);
-            var temp = result.Errors;
             if (!result.IsValid)
             {
                 throw new Exception("Something is wrong with passed arguments.");
             }
-            foreach (var error in temp)
-            {
-                var temp1 =error.ErrorMessage;
-            }
 
             using var streamReaderNew = new StreamReader(_config["FilePath"]);
             using var csvReader = new CsvReader(streamReaderNew, CultureInfo.InvariantCulture);
-            //probably not necessary
-            //csvReader.Context.TypeConverterCache.AddConverter<DateTime>(new DateTimeConverter());
             csvReader.Context.RegisterClassMap<OrderEntryMap>();
             var records = csvReader.GetRecords<OrderEntry>().ToList();
             IEnumerable<OrderEntry> results = new List<OrderEntry>();
@@ -93,7 +88,7 @@ public class CsvReaderClass
                 (request.FromWhen is not null && request.ToWhen is not null) &&
                 request.ClientCodes!.Count == 0)
             {
-                results = records.Where(x => x.OrderDate > request.FromWhen && x.ShipmentDate <= request.ToWhen).ToList();
+                results = records.Where(x => x.OrderDate > request.FromWhen && x.OrderDate <= request.ToWhen).ToList();
             }
 
             //case for client codes
