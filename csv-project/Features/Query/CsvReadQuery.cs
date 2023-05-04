7da4310fd34395d7b7f1a4dde4045ab56bde5790
nieswiezyowoc17135 <nieswiezyowoc17135@wp.pl>
2023-05-04 20:06:42 +0200

    WIP.

--- csv-project/Features/Query/CsvReadQuery.cs
+++ csv-project/Features/Query/CsvReadQuery.cs
@@ -12,68 +12,40 @@ public class CsvReadQueryValidator : AbstractValidator<CsvReadQuery>
 {
     public CsvReadQueryValidator()
     {
+
         RuleFor(x => x.OrderNumber)
+            .Cascade(CascadeMode.Stop)
             .NotNull()
-            .When(x => x.FromWhen == null && x.ToWhen == null && (x.ClientCodes == null || !x.ClientCodes.Any()))
-            .WithMessage("If OrderNumber is specified, all other arguments must be null or an empty list.");
-
-        RuleFor(x => x.FromWhen)
-            .NotNull()
-            .When(x => x.ToWhen != null && (x.OrderNumber == null || x.ClientCodes == null || !x.ClientCodes.Any()))
-            .WithMessage("If both FromWhen and ToWhen are specified, all other arguments must be null or an empty list.");
-
-        RuleFor(x => x.ToWhen)
-            .NotNull()
-            .When(x => x.FromWhen != null && (x.OrderNumber == null || x.ClientCodes == null || !x.ClientCodes.Any()))
-            .WithMessage("If both FromWhen and ToWhen are specified, all other arguments must be null or an empty list.");
+            .When(x => x.ClientCodes == null || x.FromWhen == null || x.ToWhen == null)
+            .WithMessage("OrderNumber must not be null if other arguments are null.");
 
         RuleFor(x => x.ClientCodes)
+            .Cascade(CascadeMode.Stop)
             .NotNull()
-            .When(x => x.OrderNumber == null && x.FromWhen == null && x.ToWhen == null)
-            .WithMessage("If ClientCodes is specified, all other arguments must be null.");
+            .When(x => x.OrderNumber == null || x.FromWhen == null || x.ToWhen == null)
+            .WithMessage("ClientCodes must not be null if other arguments are null.");
+
+        RuleFor(x => x.FromWhen)
+            .Cascade(CascadeMode.Stop)
+            .NotNull()
+            .When(x => x.OrderNumber == null || x.ClientCodes == null || x.ToWhen != null)
+            .WithMessage("FromWhen must not be null if ToWhen is not null and other arguments are null.");
+
+        RuleFor(x => x.ToWhen)
+            .Cascade(CascadeMode.Stop)
+            .NotNull()
+            .When(x => x.OrderNumber == null || x.ClientCodes == null || x.FromWhen != null)
+            .WithMessage("ToWhen must not be null if FromWhen is not null and other arguments are null.");
 
         RuleFor(x => x)
-            .Must(x => x.OrderNumber != null || (x.FromWhen != null && x.ToWhen != null) || x.ClientCodes != null)
-            .WithMessage("At least one of OrderNumber, FromWhen/ToWhen or ClientCodes must be specified.")
-            .Must(x => x.OrderNumber == null || (x.FromWhen == null && x.ToWhen == null && (x.ClientCodes == null || !x.ClientCodes.Any())))
-            .WithMessage("If OrderNumber is specified, all other arguments must be null or an empty list.")
+            .Cascade(CascadeMode.Stop)
+            .Must(x => x.OrderNumber != null || x.ClientCodes != null || (x.FromWhen != null && x.ToWhen != null))
+            .WithMessage("At least one of OrderNumber, ClientCodes, or FromWhen/ToWhen must be specified.");
+
+        RuleFor(x => x)
+            .Cascade(CascadeMode.Stop)
             .Must(x => x.FromWhen == null || x.ToWhen == null || x.FromWhen <= x.ToWhen)
             .WithMessage("FromWhen must be less than or equal to ToWhen.");
-        /*//rule for orderNumber
-        RuleFor(x => x)
-            .Must(x => x.FromWhen == null && x.ToWhen == null && !x.ClientCodes!.Any())
-            .When(x => !string.IsNullOrEmpty(x.OrderNumber) && x.FromWhen == null && x.ToWhen == null && x.ClientCodes!.Count > 0)
-            .WithMessage("If OrderNumber is provided, FromWhen, ToWhen and ClientCodes must be empty");
-
-        //rules for dates
-        RuleFor(x => x)
-            .Must(x => string.IsNullOrEmpty(x.OrderNumber) && !x.ClientCodes!.Any())
-            .When(x => x.FromWhen != null && x.ToWhen != null && string.IsNullOrEmpty(x.OrderNumber) && !x.ClientCodes!.Any())
-            .WithMessage("If Dates are provided, OrderNumber and ClientCoeds must be empty.");
-
-        RuleFor(x => x)
-            .Must(x => x.FromWhen != null)
-            .When(x => x.ToWhen != null)
-            .WithMessage("Fill fromWhen date.");
-
-        RuleFor(x => x)
-            .Must(x => x.ToWhen != null)
-            .When(x => x.FromWhen != null)
-            .WithMessage("Fill toWhen date.");
-
-        //rule for clientCodes
-        RuleFor(x => x)
-            .Must(x => string.IsNullOrEmpty(x.OrderNumber) && x.FromWhen == null && x.ToWhen == null)
-            .When(x => x.ClientCodes!.Count > 0 && string.IsNullOrEmpty(x.OrderNumber) && x.FromWhen == null && x.ToWhen == null)
-            .WithMessage("If ClientCodes are provided, OrderNumber and Dates must be empty.");
-
-        //Rules for all
-        RuleFor(x => x)
-            .Must(x => !string.IsNullOrEmpty(x.OrderNumber) && x.FromWhen != null && x.ToWhen != null &&
-                       x.ClientCodes!.Any())
-            .When(x => !string.IsNullOrEmpty(x.OrderNumber) && x.FromWhen != null && x.ToWhen != null &&
-                       x.ClientCodes!.Any())
-            .WithMessage("Fill all fields if u want to pass more than 1 conditional.");*/
     }
 }
 
